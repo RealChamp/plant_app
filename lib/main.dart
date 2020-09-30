3a19eac8a48af4679fab572e35897db7e63b0c0b
Matvey <matvey.w2015@mail.ru>
2020-09-30 22:02:44 +0400

    init project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Plant App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        primaryColor: primaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
